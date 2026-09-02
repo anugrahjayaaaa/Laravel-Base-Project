fa93281bfb618a3558c993773d020690ded24fe9
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-09-03 00:06:03 +0700

    fix(ui): sortable-th width stable on sort (nowrap + consistent caret)

--- resources/views/components/sortable-th.blade.php
+++ resources/views/components/sortable-th.blade.php
@@ -8,12 +8,12 @@
     $qs['dir'] = $nextDir;
 @endphp
 <th>
-    <a href="{{ url()->current() . '?' . http_build_query($qs) }}" class="text-decoration-none text-reset">
+    <a href="{{ url()->current() . '?' . http_build_query($qs) }}" class="text-decoration-none text-reset d-block text-nowrap">
         {{ $label }}
         @if ($active)
-            <i class="bi bi-caret-{{ $dir === 'asc' ? 'up' : 'down' }}-fill small"></i>
+            <i class="bi bi-caret-{{ $dir === 'asc' ? 'up' : 'down' }}-fill small ms-1"></i>
         @else
-            <i class="bi bi-caret-up small text-muted opacity-50"></i>
+            <i class="bi bi-caret-up small text-muted opacity-50 ms-1"></i>
         @endif
     </a>
 </th>
